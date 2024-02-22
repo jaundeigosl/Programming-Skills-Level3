796b36aacb5f9104563e304f5b676ce411594e22
root <juandiegosuarez00@gmail.com>
2024-02-22 11:15:43 -0400

    finished ?

--- challenge3/index.js
+++ challenge3/index.js
@@ -12,53 +12,22 @@ Fourth home: 100 square meters, 1 bedroom, 2 bathrooms
 Fifth home: 80 square meters, 1 bedroom, 1 bathroom
 The program must quote the price of the home according to: square meters, number of bedrooms, and number of bathrooms. Each bedroom adds $40, and each bathroom adds $30. Each square meter has a cost of $90.
 */
+ 
 
-import *  as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-const rl = readline.createInterface({ input, output });
-
-const ask = await rl.question("hi\n");
-rl.close();
-console.log(ask);
-
-
-
-const event = new Promise ((resolve,reject)=>{
-var name = "jun";
-if(name == "juan"){
-    resolve(name);
-    }else{
-        reject();
-    }
-});
-event.then((name)=>{
-    console.log(name);
-}).catch(()=>{
-    console.log("error");
-}).finally(()=>{
-    console.log("the end");
-})
-
-    
-
-/*let squareMeterHouses = [200 , 150, 100, 100, 80];
+let squareMeterHouses = [200 , 150, 100, 100, 80];
 let bedroomsAmount=[ 3, 2 ,2 , 1 ,1];
 let bathroomAmount=[2, 2 ,1 ,2 ,1];
 let valuePerHouse=[];
 for(let i = 0; i < 5;i++){
     valuePerHouse.push(squareMeterHouses[i] *90 + bedroomsAmount[i]*40 + bathroomAmount[i]*30);
 }
-let response;
-console.log(valuePerHouse);
-console.log("Welcome to the Real State Rent System \nSelect an option\n1-See available houses\n2-Exit");
-response = parseInt(prompt());
+let response = parseInt(prompt("Welcome to the Real State Rent System \nSelect an option\n1-See available houses\n2-Exit"));
 if(response ==1){
         console.log("Available houses\nHouse value  square meters    bedrooms   bathrooms\n");
         for(let i = 0; i < 5;i++){
-            console.log(`  ${valuePerHouse[i]}    ${squareMeterHouses[i]}     ${bedroomsAmount[i]}     ${bathroomAmount[i]}\n`)
+            console.log(`  ${valuePerHouse[i]}              ${squareMeterHouses[i]}          ${bedroomsAmount[i]}        ${bathroomAmount[i]}\n`)
         }
     }else{
 
     }
 
-*/
